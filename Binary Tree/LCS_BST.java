164cbb07d218958fefc45e3d88dbb3ea2cd4a3ee
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-11 18:02:47 +0530

    time o(logn) && space o(1)
    Example 1:
    Input:
                  5
               /    \
             4       6
            /         \
           3           7
                        \
                         8
    n1 = 7, n2 = 8
    Output: 7
    
    Example 2:
    Input:
         2
       /   \
      1     3
    n1 = 1, n2 = 3
    Output: 2

---
+++ Binary Tree/LCS_BST.java
@@ -0,0 +1,15 @@
+class BST
+{   
+	Node LCA(Node root, int n1, int n2)
+	{
+        if(root == null)  return null;
+        
+         if(root.data > n1 && root.data > n2)
+            return LCA(root.left, n1, n2);
+        else if(root.data < n1 && root.data < n2)
+            return LCA(root.right, n1, n2);
+		else
+		    return root;
+    }
+    
+}
