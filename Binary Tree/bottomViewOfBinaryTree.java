2209eac5f9f70080216ce20112cb2eee14d361b3
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-14 04:51:01 +0530

    time o(n) and space o(n)
    Example 1:
    Input:
           1
         /   \
        3     2
    Output: 3 1 2
     
    Example 2:
    Input:
             10
           /    \
          20    30
         /  \
        40   60
    Output: 40 20 60 30

---
+++ Binary Tree/bottomViewOfBinaryTree.java
@@ -0,0 +1,52 @@
+class Pair{
+    Node node;
+    int hd;
+    
+    Pair(Node node, int hd){
+        this.node = node;
+        this.hd = hd;
+    }
+}
+
+
+class Tree
+{
+    static ArrayList<Integer> bottomView(Node root)
+    {
+        if(root == null) 
+            return new ArrayList<>();
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Pair> queue = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        queue.add(new Pair(root, 0));
+        
+        while(!queue.isEmpty()){
+            Pair front = queue.poll();
+            Node curr = front.node;
+            int dis = front.hd;
+            
+            // if(!map.containsKey(dis))    // remove this line and we will get the bottom view
+                map.put(dis, curr);
+                
+            if(curr.left != null)
+                queue.add(new Pair(curr.left, dis-1));
+            
+            if(curr.right != null)
+                queue.add(new Pair(curr.right, dis+1));
+            
+        }
+        
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(Map.Entry<Integer, Node> entry : map.entrySet()){
+            min = Math.min(min, entry.getKey());
+            max = Math.max(max, entry.getKey());
+        }
+            
+        for(int i=min; i<=max; i++)
+            ans.add(map.get(i).data);
+   
+        return ans;
+    }
+    
+}
