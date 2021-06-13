23911c4d0a52fc84f891b02d28a9f41d55b631e2
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-14 04:08:59 +0530

    solved using level order and DFS (preorder traversal)
    Example 1:
    Input: root = [1,2,3,null,5,null,4]
    Output: [1,3,4]
    Example 2:
    Input: root = [1,null,3]
    Output: [1,3]
    
    Example 3:
    Input: root = []
    Output: []

--- Binary Tree/rightViewOfBinaryTree.java
+++ Binary Tree/rightViewOfBinaryTree.java
@@ -49,3 +49,44 @@ class Solution {
 // method 2
 // using preorder traversal
 // time o(n) and space o(h), h is the height of the binary tree
+
+
+class Solution {
+        
+    boolean check[];
+    List<Integer> rightSideView(TreeNode root)
+    { 
+        if(root == null)
+            return new ArrayList<>();
+        
+     
+      int height = depth(root);
+      check = new boolean[height];
+      
+      ArrayList<Integer> list = new ArrayList<>();
+      leftView(root, list, 0);
+      
+      return list;
+    }
+    
+    private void leftView(TreeNode root, ArrayList<Integer> list, int level){
+        if(root == null) return;
+        
+        if(check[level] == false){
+            list.add(root.val);
+            check[level] = true;
+        }
+        
+        leftView(root.right, list, level+1);
+        leftView(root.left, list, level+1);
+        
+        return;
+    }
+    
+    private int depth(TreeNode root){
+        if(root == null)
+            return 0;
+        
+        return 1 + Math.max(depth(root.left), depth(root.right));
+    }
+}
