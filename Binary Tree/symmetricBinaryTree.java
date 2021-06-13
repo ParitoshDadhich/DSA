363e3a6964487c76abac69a62a375d99b74863d1
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-13 12:49:32 +0530

    solved using recursive and iterative approach
    Example 1:
    Input:
             5
           /   \
          1     1
         /       \
        2         2
    Output: True
    Explanation: Tree is mirror image of
    itself i.e. tree is symmetric
    
    Example 2:
    Input:
             5
           /   \
          10     10
         /  \     \
        20  20     30
    Output: False

--- Binary Tree/symmetricBinaryTree.java
+++ Binary Tree/symmetricBinaryTree.java
@@ -1,3 +1,8 @@
+// method 1
+// using recursion
+// time o(n) and space o(h), where h is the height of internal stack which is used during recursion calls;
+
+
 class GfG
 {
     public static boolean isSymmetric(Node root)
@@ -20,5 +25,34 @@ class GfG
                 return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
             
         return false;
+    }
+}
+
+
+// method 2
+// using iterative approach
+// time o(n) and space (n)
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+            if(t1.val != t2.val) return false;
+            
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        
+        return true;
     }
 }
