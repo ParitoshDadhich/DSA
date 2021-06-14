f14dc41e02026434cecb7dd99b4f1bf62eeb2911
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-15 05:05:11 +0530

    time o(logn)
    Example 1:
    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
    Output: 6
    Explanation: The LCA of nodes 2 and 8 is 6.
    
    Example 2:
    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
    Output: 2
    Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
    
    Example 3:
    Input: root = [2,1], p = 2, q = 1
    Output: 2

---
+++ Binary Search Tree/LCAofBST.java
@@ -0,0 +1,53 @@
+// method1
+// recursive
+
+
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)  return null;
+        
+         if(root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if(root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+		else
+		    return root;
+    }
+}
+ 
+// method2
+// itetative
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Value of p
+        int pVal = p.val;
+
+        // Value of q;
+        int qVal = q.val;
+
+        // Start from the root node of the tree
+        TreeNode node = root;
+
+        // Traverse the tree
+        while (node != null) {
+
+            // Value of ancestor/parent node.
+            int parentVal = node.val;
+
+            if (pVal > parentVal && qVal > parentVal) {
+                // If both p and q are greater than parent
+                node = node.right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                // If both p and q are lesser than parent
+                node = node.left;
+            } else {
+                // We have found the split point, i.e. the LCA node.
+                return node;
+            }
+        }
+        return null;
+    }
+}
