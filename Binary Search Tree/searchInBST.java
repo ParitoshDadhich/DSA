58215997237223c5c9517c2e194db88ad3fa6442
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-15 04:32:23 +0530

    Create searchInBST.java

---
+++ Binary Search Tree/searchInBST.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null)   
+            return null;
+        
+        if(root.val == val)
+            return root;
+        
+        if(root.val > val)
+            return searchBST(root.left, val);
+        else
+            return searchBST(root.right, val);
+    }
+}
