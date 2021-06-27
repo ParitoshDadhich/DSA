24f337da93143dc03e4c16ccb3225d460a7b9efa
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-27 17:45:29 +0530

    Update minimumPlatform.java

--- Greedy Algo/minimumPlatform.java
+++ Greedy Algo/minimumPlatform.java
@@ -1,4 +1,29 @@
 
+// brute force
+
+    int plat_needed = 1, result = 1;
+
+    // run a nested  loop to find overlap
+    for (int i = 0; i < n; i++) {
+        // minimum platform
+        plat_needed = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            // check for overlap
+            if ((arr[i] >= arr[j] && arr[i] <= dep[j]) || (arr[j] >= arr[i] && arr[j] <= dep[i]))
+                plat_needed++;
+        }
+
+        // update result
+        result = Math.max(result, plat_needed);
+    }
+
+    return result;
+
+
+
+// method 2;
+
 class Solution
 {
     //Function to find the minimum number of platforms required at the
