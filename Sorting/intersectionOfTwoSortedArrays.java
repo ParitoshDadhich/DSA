f8dc5432e89e2471b707d507e9b63e69e8cd64bd
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-28 08:48:33 +0530

    solved using different approaches
    input: 
    [1,2,2,1]
    [1,1]
    
    output:
    [1]

--- Sorting/intersectionOfTwoSortedArrays.java
+++ Sorting/intersectionOfTwoSortedArrays.java
@@ -61,3 +61,39 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// using merging idea
+// time o(n1 + n2) and space o(1)
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int i = 0, j = 0;
+        while(i<n1 && j<n2){
+            if(i>0 && nums1[i] == nums1[i-1]){i++; continue;}
+            
+            if(nums1[i] < nums2[j])
+                i++;
+            else if(nums1[i] > nums2[j])
+                j++;
+            else{
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        int n = list.size();
+        int ans[] = new int[n];
+        for(i=0; i<n; i++)
+            ans[i] = list.get(i);
+        
+        
+        return ans;
+    }
+}
