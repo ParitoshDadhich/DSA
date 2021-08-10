51b689ea49e17ea03147d89fc2a4ee9d0056a2aa
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-08-11 00:47:43 +0530

    time o(n2) and space o(n2)
    input:
    arr[] = 2, 3, 5, 6, 8, 10
    sum = 10

---
+++ DP/countSubsetSumWithAGivenSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraySum(int[] arr, int sum) {
+        int n = arr.length;
+        int t[][] = new int[n+1][sum+1];
+        
+        for(int i=0; i<=n; i++)
+            t[i][0] = 1;
+        for(int j=1; j<=sum; j++)
+            t[0][j] = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(arr[i-1] <= j)
+                    t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][sum];
+    }
+}
