b0d9e78417a7abcabbd7c0a02ee378b5e9e14ddb
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-08-12 19:39:01 +0530

    time o(n2) and spce o(n2)
    Example 1:
    Input: str1 = "heap", str2 = "pea"
    Output: 3
    Explanation: 2 deletions and 1 insertion
    p and h deleted from heap. Then, p is 
    inserted at the beginning One thing to 
    note, though p was required yet it was 
    removed/deleted first from its position 
    and then it is inserted to some other 
    position. Thus, p contributes one to the 
    deletion_count and one to the 
    insertion_count.
    
    
    Example 2:
    Input : str1 = "geeksforgeeks"
    str2 = "geeks"
    Output: 8
    Explanation: 8 insertions

---
+++ DP/LCS/minNumberOfInsertionsAndDeletion.java
@@ -0,0 +1,23 @@
+class Solution
+{
+	public int minOperations(String str1, String str2) 
+	{ 
+	    int n = str1.length();
+	    int m = str2.length();
+	    
+	    int t[][] = new int[n+1][m+1];
+	    
+	    for(int i=0; i<=n; i++){
+	        for(int j=0; j<=m; j++){
+	            if(i==0 || j==0)
+	                t[i][j] = 0;
+	           else if(str1.charAt(i-1) == str2.charAt(j-1))
+	                t[i][j] = 1 + t[i-1][j-1];
+	           else
+	                t[i][j] = Math.max(t[i][j-1], t[i-1][j]);
+	        }
+	    }
+	    
+	    return m+n-2*t[n][m];
+	} 
+}
