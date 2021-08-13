3379308506bf699401f27655b80db24afc781cd6
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-08-13 19:01:26 +0530

    solved with different approaches
    Example 1:
    Input: str = "ababbbabbababa"
    Output: 3
    Explaination: After 3 partitioning substrings 
    are "a", "babbbab", "b", "ababa".
    
    Example 2:
    Input: str = "aaabba"
    Output: 1
    Explaination: The substrings after 1
    partitioning are "aa" and "abba".

---
+++ DP/MCM/Palindrome/palindromePatitioning.java
@@ -0,0 +1,102 @@
+// method 1
+// recursive solution
+// time o(2^N) and space o(1)
+
+class Solution{
+    static int palindromicPartition(String str)
+    {
+        int n = str.length();
+        int i = 0;
+        int j = n - 1;
+        
+        return helper(str, i, j);
+    }
+    
+    private static int helper(String str, int i, int j){
+        // base case
+        if(i >= j)
+            return 0;
+        if(isPalindrome(str, i, j))
+            return 0;
+            
+        int ans = Integer.MAX_VALUE;
+        for(int k=i; k<=j-1; k++){
+            int tempAns = 1 + helper(str, i, k) + helper(str, k+1, j);
+            
+            if(ans > tempAns)
+                ans = tempAns;
+        }  
+        return ans;
+    }
+    
+    
+    private static boolean isPalindrome(String str, int i, int j){
+        
+        if(i == j)
+            return true;
+        
+        if(i>j || str.charAt(i) != str.charAt(j))
+            return false;
+        
+        if(i + 1 < j)
+            return isPalindrome(str, i+1, j-1);
+            
+        return true;
+    }
+    
+}
+
+
+// method 2
+// memoization
+// time o(n2) and space o(n2)
+
+class Solution{
+    static int palindromicPartition(String str)
+    {
+        int n = str.length();
+        
+        int t[][] = new int[n][n];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                t[i][j] = -1;
+        
+        return helper(str, 0, n-1, t);
+    }
+    
+    private static int helper(String str, int i, int j, int t[][]){
+        // base case
+        if(i >= j)
+            return 0;
+        if(isPalindrome(str, i, j))
+            return 0;
+            
+        if(t[i][j] != -1)
+            return t[i][j];
+            
+        int ans = Integer.MAX_VALUE;
+        for(int k=i; k<=j-1; k++){
+            int tempAns = 1 + helper(str, i, k, t) + helper(str, k+1, j, t);
+            
+            if(ans > tempAns)
+                ans = tempAns;
+        }  
+        return t[i][j] = ans;
+    }
+    
+    
+    private static boolean isPalindrome(String str, int i, int j){
+        
+        if(i == j)
+            return true;
+        
+        if(i>j || str.charAt(i) != str.charAt(j))
+            return false;
+        
+        if(i + 1 < j)
+            return isPalindrome(str, i+1, j-1);
+            
+        return true;
+    }
+    
+}
