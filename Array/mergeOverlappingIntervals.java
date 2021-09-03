c6e784a744171fea6b8d5ee679029e46d12298ad
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-09-04 01:18:58 +0530

    solved with different approaches
    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
    Output: [[1,6],[8,10],[15,18]]
    Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].

--- Array/mergeOverlappingIntervals.java
+++ Array/mergeOverlappingIntervals.java
@@ -1,4 +1,47 @@
 // method 1
+// time o(n2) and space o(n)
+class Solution {
+    public int[][] merge(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> (a[0] - b[0]));
+        List<int []> list = new ArrayList<>();
+        
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            int start = arr[i][0];
+            int end = arr[i][1];
+            int len = list.size();
+            
+            if(len != 0 && list.get(len-1)[1] > start)
+                continue;
+            
+            for(int j=i+1; j<n; j++){
+                if(end > arr[j][0]){
+                    if(end < arr[j][1])
+                        end = arr[j][1];
+                } else
+                    break;
+            }
+            
+            int temp[] = new int[2];
+            temp[0] = start;
+            temp[1] = end;
+            
+            if(len != 0 && list.get(len-1)[1] == start){
+                temp[0] = list.get(len-1)[0];
+                list.remove(len-1);
+                list.add(temp);
+            } else
+                list.add(temp);
+            
+        }
+         return list.toArray(new int[list.size()][]);
+    }
+}
+
+
+
+
+// method 2
 // time o(nlogn) and space o(n)
 
 class Solution {
