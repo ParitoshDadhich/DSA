19fc260ddf8748f938d8acb34adeaa2d9980929a
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-12-30 13:15:37 +0530

    Update checkPermutations.java

--- foundation/string/checkPermutations.java
+++ foundation/string/checkPermutations.java
@@ -27,3 +27,16 @@ public class Solution {
     }
 }
         
+
+
+// method 2
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }
+}
