b11420a4cc1e7ff9efa97f7c8c181443692710a5
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-12-30 13:26:11 +0530

    Update reverseSringWordByWord.java

--- foundation/string/reverseSringWordByWord.java
+++ foundation/string/reverseSringWordByWord.java
@@ -25,3 +25,35 @@ public class Solution {
         return ans;
 	}
 }
+
+
+
+// by using string builder
+
+public class Solution {
+	public static String reverseWordWise(String s) {
+		
+        int i = s.length() - 1;
+        StringBuilder ans = new StringBuilder();
+        while(i>=0){
+            // taking care of spaces
+            while(i>=0 && s.charAt(i) == ' ')
+                i--;
+            
+            int j = i;
+            if(i<0)
+                break;
+        	
+            // main logic
+            while(i>=0 && s.charAt(i) != ' ')
+            	i--;
+        	if(ans.capacity() == 0)
+                ans.append(s.substring(i+1, j+1));
+            else{
+                ans.append(" " + s.substring(i+1, j+1));
+            }
+        }
+	
+        return ans.toString().trim();
+	}
+}
