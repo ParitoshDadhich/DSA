136ca3f866bd6627ed2d243da79ab6696295889e
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2022-09-12 07:41:36 +0530

    SOLVED WITH DIFFERENT APPROACHES

--- Binary Tree/checkForBalancedBinaryTree.java
+++ Binary Tree/checkForBalancedBinaryTree.java
@@ -1,3 +1,57 @@
+// Brute force
+// time o(n2)
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        
+        int lh = height(root.left);
+        int rh = height(root.right);
+        
+        if(Math.abs(lh-rh) > 1) return false;
+        
+        boolean lTree = isBalanced(root.left);
+        boolean rTree = isBalanced(root.right);
+        
+        return lTree != rTree ? false : (lTree == false ? false : true);
+    }
+    
+    private int height(TreeNode root){
+        if(root == null) return 0;
+        
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+}
+
+// better optimal sol
+// time o(n)
+
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+    
+    private int dfsHeight(TreeNode root){
+        if(root == null) return 0;
+        
+        int leftH = dfsHeight(root.left);
+        if(leftH == -1) return -1;
+        
+        int rightH = dfsHeight(root.right);
+        if(rightH == -1) return -1;
+        
+        if(Math.abs(leftH - rightH) > 1) return -1;
+        
+        return 1 + Math.max(leftH, rightH);
+    }
+    
+}
+
+
+
+// another way of writing sol using pair class
+// time o(n)
 class Pair{
     int height = 0;
     boolean isBalance = true;
