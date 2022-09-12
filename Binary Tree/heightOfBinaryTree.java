3617ae24ab5f73ae6958dfc7cb6d8c773981642e
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2022-09-12 06:35:10 +0530

    Update heightOfBinaryTree.java

--- Binary Tree/heightOfBinaryTree.java
+++ Binary Tree/heightOfBinaryTree.java
@@ -1,9 +1,38 @@
- 
+// Recursive solution; 
+
+
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null)
             return 0;
         
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+
+// Iterative sol
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int height = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            height++;
+            int len = queue.size();
+            for(int i=0; i<len; i++){
+                TreeNode node = queue.poll();
+                if(node.left != null)
+                    queue.offer(node.left);
+                if(node.right != null)
+                queue.offer(node.right);
+            }
+        }
+        
+        return height;
     }
 }
