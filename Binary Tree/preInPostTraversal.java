fca590e7f47d9b521cd215f7784310f452559e85
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2022-09-12 05:52:37 +0530

    time o(n) and space o(n)

---
+++ Binary Tree/preInPostTraversal.java
@@ -0,0 +1,92 @@
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+class Pair {
+    TreeNode node;
+    int num;
+    Pair(TreeNode _node, int _num) {
+        num = _num;
+        node = _node;
+    }
+}
+public class TUF {
+    public static void allTraversal(TreeNode root, List < Integer > pre, List < Integer > in , List < Integer > post) {
+        Stack < Pair > st = new Stack < Pair > ();
+        st.push(new Pair(root, 1));
+        
+        if (root == null) return;
+
+        while (!st.isEmpty()) {
+            Pair it = st.pop();
+
+            // this is part of pre
+            // increment 1 to 2 
+            // push the left side of the tree
+            if (it.num == 1) {
+                pre.add(it.node.val);
+                it.num++;
+                st.push(it);
+
+                if (it.node.left != null) {
+                    st.push(new Pair(it.node.left, 1));
+                }
+            }
+
+            // this is a part of in 
+            // increment 2 to 3 
+            // push right 
+            else if (it.num == 2) { in .add(it.node.val);
+                it.num++;
+                st.push(it);
+
+                if (it.node.right != null) {
+                    st.push(new Pair(it.node.right, 1));
+                }
+            }
+            // don't push it back again 
+            else {
+                post.add(it.node.val);
+            }
+        }
+ 
+
+    }
+    public static void main(String args[]) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        List < Integer > pre = new ArrayList < > ();
+        List < Integer > in = new ArrayList < > ();
+        List < Integer > post = new ArrayList < > ();
+        allTraversal(root, pre, in , post);
+
+        System.out.println("The preorder Traversal is : ");
+        for (int nodeVal: pre) {
+            System.out.print(nodeVal + " ");
+        }
+        System.out.println();
+        System.out.println("The inorder Traversal is : ");
+        for (int nodeVal: in ) {
+            System.out.print(nodeVal + " ");
+        }
+        System.out.println();
+        System.out.println("The postorder Traversal is : ");
+        for (int nodeVal: post) {
+            System.out.print(nodeVal + " ");
+        }
+        System.out.println();
+    }
+}
