b944028b048b669bd630082bcf42c00de3838ae5
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2022-09-16 03:37:05 +0530

    solved with different approaches

--- Binary Tree/binaryTreeZigZagLevelOrderTraversal.java
+++ Binary Tree/binaryTreeZigZagLevelOrderTraversal.java
@@ -84,3 +84,37 @@ class Solution {
         return ans;
     }
 }
+
+// m3
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        zigzagHelp(root, 0, res);
+        return res;
+        
+    }
+    
+    public void zigzagHelp(TreeNode root, int level, List<List<Integer>> list) {
+        
+        if (root == null) {
+            return;
+        }
+        
+        if (list.size() <= level) {
+            list.add(new ArrayList<>());      
+        } 
+        
+        if (level % 2 == 0) {
+            list.get(level).add(root.val);
+        }
+        else {
+           list.get(level).add(0, root.val);   
+        }
+        
+        zigzagHelp(root.left, level + 1, list);
+        zigzagHelp(root.right, level + 1, list);
+        
+    }
+}
